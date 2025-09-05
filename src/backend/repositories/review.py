52824e490595a7178e531e48f9956d3abc491725
LordMilton <dufour.bradley@gmail.com>
2025-09-05 14:17:00 -0400

    Add new methods of retrieving reviews
    Reviews can be retrieved in order from best to worst reviews.
    
    Best-sorted and latest-sorted reviews can be filtered by user id and/or
    restaurant name. New sorting methods are available on appropriate
    endpoints with user id and restaurant-name as optional query params

--- src/backend/repositories/review.py
+++ src/backend/repositories/review.py
@@ -6,6 +6,12 @@ postgres_details = get_postgres_server_details("../../config")
   
 engine  = create_engine(f"postgresql://{postgres_details['username']}:{postgres_details['password']}@{postgres_details['host']}:{postgres_details['port']}/foodreview")
 
+# These functions should have ground truth on these items but the query parameters need to set the defaults at run-time
+DEFAULT_REVIEW_LIMIT = 5
+DEFAULT_USER_ID = -1
+DEFAULT_RESTAURANT_NAME = ""
+
+# ADDING REVIEWS
 def add_new_review(review: NewReviewDetails):
   with engine.connect() as conn:
     conn.execute(
@@ -15,12 +21,13 @@ def add_new_review(review: NewReviewDetails):
           rating, date, meal_items, meal_cost,
           review_text)
         VALUES
-          (1, :restaurant_name, :headline,
+          (:user_id, :restaurant_name, :headline,
           :rating, :date, :meal_items, :meal_cost,
           :review_text)
         """
           ),
         {
+          "user_id": review.user_id,
           "restaurant_name": review.restaurant_name,
           "headline": review.headline,
           "rating": review.burger_rating,
@@ -33,19 +40,90 @@ def add_new_review(review: NewReviewDetails):
 
     conn.commit()
 
-def get_latest_reviews(limit: int = 5) -> list[ReviewDetails]:
+# GETTING REVIEWS
+## LATEST REVIEWS
+def get_latest_reviews(limit: int, user_id: int, restaurant_name: str) -> list[ReviewDetails]:
+  # If user_id is provided, remove it from the columns returned? Same for restaurant_name?
+  query_string = """
+    SELECT id,
+      user_id,
+      restaurant_name,
+      headline,
+      rating as burger_rating,
+      date as review_date,
+      meal_items,
+      meal_cost,
+      review_text
+    FROM reviews
+    """
+    
+  where_clauses = []
+  if(restaurant_name != DEFAULT_RESTAURANT_NAME):
+    where_clauses.append("restaurant_name = :restaurant_name")
+  if(user_id > 0):
+    where_clauses.append("user_id = :user_id")
+  query_string += craft_where_clause(where_clauses)
+  
+  query_string += """
+    ORDER BY created_at DESC
+    LIMIT :limit
+    """
+  
   with engine.connect() as conn:
     result = conn.execute(
-      text("""
-          SELECT id, user_id, restaurant_name , headline,
-            rating as burger_rating, date as review_date, meal_items, meal_cost, review_text
-          FROM reviews
-          ORDER BY created_at DESC
-          LIMIT :limit
-          """),
+      text(query_string),
           {
-            "limit": limit
+            "limit": limit,
+            "user_id": user_id,
+            "restaurant_name": restaurant_name
           }
     )
 
     return [ReviewDetails(**row) for row in result.mappings()]
+
+## BEST REVIEWS    
+def get_best_reviews(limit: int = DEFAULT_REVIEW_LIMIT, user_id: int = -1, restaurant_name: str = ""):
+  # If user_id is provided, remove it from the columns returned? Same for restaurant_name
+  query_string = """
+    SELECT id,
+      user_id,
+      restaurant_name,
+      headline,
+      rating as burger_rating,
+      date as review_date,
+      meal_items,
+      meal_cost,
+      review_text
+    FROM reviews
+    """
+
+  where_clauses = []
+  if(restaurant_name != DEFAULT_RESTAURANT_NAME):
+    where_clauses.append("restaurant_name = :restaurant_name")
+  if(user_id > 0):
+    where_clauses.append("user_id = :user_id")
+  query_string += craft_where_clause(where_clauses)
+  
+  query_string += """
+    ORDER BY rating DESC
+    LIMIT :limit
+    """
+  
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(query_string),
+          {
+            "limit": limit,
+            "user_id": user_id,
+            "restaurant_name": restaurant_name
+          }
+    )
+    
+    return [ReviewDetails(**row) for row in result.mappings()]
+  
+def craft_where_clause(clauses):
+  if not clauses:
+    return ""
+  
+  conditions = " AND ".join(clauses)
+  return f"WHERE {conditions}"
--- src/backend/serve.py
+++ src/backend/serve.py
@@ -1,5 +1,6 @@
 from models.review_details import NewReviewDetails, ReviewDetails
 import repositories.review
+from repositories.review import DEFAULT_RESTAURANT_NAME, DEFAULT_REVIEW_LIMIT, DEFAULT_USER_ID
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -25,9 +26,14 @@ async def post_new_review(new_review_details: NewReviewDetails):
   return {"test": f"thx for the review about {new_review_details.restaurant_name}"}
 
 @router.get("/latest-reviews")
-async def get_latest_reviews() -> list[ReviewDetails]:
-  latest_reviews = repositories.review.get_latest_reviews()
+async def get_latest_reviews(limit: int = repositories.review.DEFAULT_REVIEW_LIMIT, user_id: int = DEFAULT_USER_ID, restaurant_name: str = DEFAULT_RESTAURANT_NAME) -> list[ReviewDetails]:
+  latest_reviews = repositories.review.get_latest_reviews(limit, user_id, restaurant_name)
   return latest_reviews
+
+@router.get("/best-reviews")
+async def get_best_reviews(limit: int = repositories.review.DEFAULT_REVIEW_LIMIT, user_id: int = DEFAULT_USER_ID, restaurant_name: str = DEFAULT_RESTAURANT_NAME) -> list[ReviewDetails]:
+  best_reviews = repositories.review.get_best_reviews(limit, user_id, restaurant_name)
+  return best_reviews
 
 app.include_router(router)
 
